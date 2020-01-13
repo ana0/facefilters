26bfd86b0cbcb23ab7a61cd620f66f9ccafa1dda
anon <sarah.anne.friend@gmail.com>
2020-01-13 16:38:29 -0500

    Move gitignore, small script but isn't working

--- src/main.py
+++ src/main.py
@@ -1,20 +1,51 @@
 import os
+import cv2
 import openface.openface as openface
 
 fileDir = os.path.dirname(os.path.realpath(__file__))
-modelDir = os.path.join(fileDir, '..', 'openface', 'models')
+modelDir = os.path.join(fileDir, 'openface', 'models')
+filesDir = os.path.join(fileDir, 'blobframes')
 dlibModelDir = os.path.join(modelDir, 'dlib')
 openfaceModelDir = os.path.join(modelDir, 'openface')
+dlibFacePredictor = os.path.join(
+    dlibModelDir,
+    "shape_predictor_68_face_landmarks.dat")
+
+file = os.path.join(filesDir, 'Sequence 02000.jpg')
 
 networkModel = os.path.join(openfaceModelDir, 'nn4.small2.v1.t7')
 imgDim = 96
 cuda = True
 
+align = openface.AlignDlib(dlibFacePredictor)
+net = openface.TorchNeuralNet(
+    networkModel,
+    imgDim=imgDim,
+    cuda=cuda)
+
+def loadImg(bgrImg):
+    rgbImg = cv2.cvtColor(bgrImg, cv2.COLOR_BGR2RGB)
+    bb = align.getAllFaceBoundingBoxes(rgbImg)
+    if bb is None:
+        return None
+    alignedFaces = []
+    for box in bb:
+        alignedFaces.append(
+            align.align(
+                imgDim,
+                rgbImg,
+                box,
+                landmarkIndices=openface.AlignDlib.OUTER_EYES_AND_NOSE))
+    if alignedFaces is None:
+        raise Exception("Unable to align the frame")
+    reps = []
+    for alignedFace in alignedFaces:
+        reps.append(net.forward(alignedFace))
+    return (reps)
+
 def run():
-    net = openface.TorchNeuralNet(
-        networkModel,
-        imgDim=imgDim,
-        cuda=cuda)
+    img = cv2.imread(file, 0)
+    loadImg(img)
 
 if __name__ == "__main__":
     run()
